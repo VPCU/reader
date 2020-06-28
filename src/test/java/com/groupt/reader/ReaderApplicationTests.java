e2b78be3ab3bc6426ecc0fafad824790d57c5a5b
mhr <hzi@upc.com>
2020-06-28 16:09:30 +0800

    first commit

---
+++ src/test/java/com/groupt/reader/ReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupt.reader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
