fa4fb683867257fd301c73710edabad2c8193103
王昭 <1447731440@qq.com>
2020-07-01 11:24:02 +0800

    书评广场首页+无限滚动组件，未连接后端

--- front/src/layouts/MainLayout.vue
+++ front/src/layouts/MainLayout.vue
@@ -1,21 +1,41 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
+  <q-layout view="hHh LpR fFf" class="bg-grey-3">
+    <q-header class="bg-grey-3 text-grey-9" reveal height-hint="60">
+      <q-toolbar class="GPLAY__toolbar text-grey-6">
         <q-btn
+          v-if="this.$q.platform.is.mobile || !leftDrawerOpen"
           flat
           dense
           round
-          icon="menu"
-          aria-label="Menu"
           @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+          icon="menu"
+          class="q-mr-sm"
         />
+        <q-space />
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+        <div class="GPLAY__toolbar-input-container row no-wrap">
+          <q-input dense outlined square v-model="search" placeholder="Search" class="bg-white col" />
+          <q-btn  class="GPLAY__toolbar-input-btn" width = "5px" color="primary" icon="search" unelevated />
+        </div>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <q-space />
+
+        <div class="q-pl-md q-gutter-sm row no-wrap items-center">
+          <q-btn round dense flat color="grey-8" icon="notifications">
+            <q-badge color="red" text-color="white" floating>
+              2
+            </q-badge>
+            <q-tooltip>Notifications</q-tooltip>
+          </q-btn>
+
+          <q-btn round flat>
+            <q-avatar size="26px">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+            </q-avatar>
+            <q-tooltip>Account</q-tooltip>
+          </q-btn>
+        </div>
       </q-toolbar>
     </q-header>
 
@@ -23,20 +43,72 @@
       v-model="leftDrawerOpen"
       show-if-above
       bordered
-      content-class="bg-grey-1"
+      content-class="bg-grey-3 text-grey-7"
+      :width="200"
     >
       <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
+
+        <q-item clickable class="GPLAY__drawer-link bg-grey-10 text-grey-3">
+          <q-item-section avatar class="text-grey-1">
+            <q-icon name="weekend" />
+          </q-item-section>
+          <q-item-section class="text-grey-3">
+            <q-item-label>Entertainment</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable class="GPLAY__drawer-link GPLAY__drawer-link--apps">
+          <q-item-section avatar class="bg-green-7 text-grey-1 text-center">
+            <q-icon name="android" />
+          </q-item-section>
+          <q-item-section class="apps-text">
+            <q-item-label>Apps</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable class="GPLAY__drawer-link GPLAY__drawer-link--movies">
+          <q-item-section avatar class="movies-icon bg-red-7 text-grey-1 text-center">
+            <q-icon name="local_movies" />
+          </q-item-section>
+          <q-item-section class="movies-text">
+            <q-item-label>Movies & TV</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable class="GPLAY__drawer-link GPLAY__drawer-link--music">
+          <q-item-section avatar class="music-icon bg-orange-7 text-grey-1 text-center">
+            <q-avatar size="22px" color="white" text-color="orange-7" icon="music_note" />
+          </q-item-section>
+          <q-item-section class="music-text">
+            <q-item-label>Music</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable class="GPLAY__drawer-link GPLAY__drawer-link--books">
+          <q-item-section avatar class="books-icon bg-blue-7 text-grey-1 text-center">
+            <q-icon name="book" />
+          </q-item-section>
+          <q-item-section class="books-text">
+            <q-item-label>Books</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable class="GPLAY__drawer-link GPLAY__drawer-link--devices">
+          <q-item-section avatar class="devices-icon bg-blue-grey-7 text-grey-1 text-center">
+            <q-icon name="devices" />
+          </q-item-section>
+          <q-item-section class="devices-text">
+            <q-item-label>Devices</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-separator class="q-mb-md" />
+
+        <q-item v-for="link in links1" :key="link.text" v-ripple dense clickable class="GPLAY__drawer-item">
+          <q-item-section class="text-grey-8">
+            <q-item-label>{{ link.text }}</q-item-label>
+          </q-item-section>
+        </q-item>
       </q-list>
     </q-drawer>
 
@@ -47,63 +119,76 @@
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
-
 export default {
-  name: 'MainLayout',
-
-  components: {
-    EssentialLink
-  },
-
+  name: 'GooglePlayLayout',
   data () {
     return {
       leftDrawerOpen: false,
-      essentialLinks: [
-        {
-          title: 'Docs',
-          caption: 'quasar.dev',
-          icon: 'school',
-          link: 'https://quasar.dev'
-        },
-        {
-          title: 'Github',
-          caption: 'github.com/quasarframework',
-          icon: 'code',
-          link: 'https://github.com/quasarframework'
-        },
-        {
-          title: 'Discord Chat Channel',
-          caption: 'chat.quasar.dev',
-          icon: 'chat',
-          link: 'https://chat.quasar.dev'
-        },
-        {
-          title: 'Forum',
-          caption: 'forum.quasar.dev',
-          icon: 'record_voice_over',
-          link: 'https://forum.quasar.dev'
-        },
-        {
-          title: 'Twitter',
-          caption: '@quasarframework',
-          icon: 'rss_feed',
-          link: 'https://twitter.quasar.dev'
-        },
-        {
-          title: 'Facebook',
-          caption: '@QuasarFramework',
-          icon: 'public',
-          link: 'https://facebook.quasar.dev'
-        },
-        {
-          title: 'Quasar Awesome',
-          caption: 'Community Quasar projects',
-          icon: 'favorite',
-          link: 'https://awesome.quasar.dev'
-        }
+      search: '',
+      storage: 0.26,
+      links1: [
+        { text: 'Account' },
+        { text: 'Payment methods' },
+        { text: 'My subscriptions' },
+        { text: 'Redeem' },
+        { text: 'Buy gift card' },
+        { text: 'My wishlist' },
+        { text: 'My Play activity' },
+        { text: 'Parent guide' }
       ]
     }
   }
 }
+
 </script>
+<style lang="sass">
+  .GPLAY
+    &__toolbar
+      height: 60px
+    &__logo
+      width: 183px
+      height: 39px
+    &__toolbar-input-container
+      min-width: 100px
+      width: 55%
+    &__toolbar-input-btn
+      border-radius: 0
+      max-width: 30px
+      width: 100%
+    &__drawer-link
+    .q-item__section--avatar
+      margin: -8px 0 -8px -16px
+      padding: 8px 0 8px 16px
+      .q-item__section--main
+        margin: -8px -16px -8px 16px
+        padding: 8px 16px 8px 2px
+        font-size: 18px
+        font-weight: 300
+      &--apps, &--movies, &--music, &--books, &--devices
+        background: #f5f5f5!important
+        &:hover
+          color: #eee !important
+      &--apps:hover
+        background: #43a047!important
+      &--movies:hover
+        background: #e53935!important
+      &--music:hover
+        background: #fb8c00!important
+      &--books:hover
+        background: #1e88e5!important
+      &--devices:hover
+        background: #546e7a!important
+    &__drawer-item
+      padding: 6px 12px 6px 23px
+    &__sticky
+      min-height: 49px
+      border-bottom: 1px solid rgba(0,0,0,0.12)
+    &__sticky-help
+      border: 1px solid #ccc
+      padding-left: 8px
+      padding-right: 8px
+    &__sticky-settings
+      padding-left: 17px
+      padding-right: 17px
+      border: 1px solid #ccc
+</style>
