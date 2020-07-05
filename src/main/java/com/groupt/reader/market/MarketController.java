6cf886794f87ee87ed2d1283181cd198f2670fd9
mhr <hzi@upc.com>
2020-07-05 12:31:28 +0800

    Update MarketController.java

--- src/main/java/com/groupt/reader/market/MarketController.java
+++ src/main/java/com/groupt/reader/market/MarketController.java
@@ -20,14 +20,26 @@ public class MarketController {
     @GetMapping("/market")
     public Object get() {
         List<BookEntity> books = bookRepository.findAll();
-        Goods goods = new Goods();
+        Map<String, Goods> dic = new HashMap<>();
         for(BookEntity book: books) {
+            if(book.getDetail() == null) continue;
             Good b = new Good(book);
-            goods.add(b);
+            if(!dic.containsKey(book.getDetail())) {
+                Goods g = new Goods();
+                g.setType(book.getDetail());
+                dic.put(book.getDetail(), g);
+            }
+            dic.get(book.getDetail()).add(b);
         }
-        GoodsList goodsList = new GoodsList();
-        goodsList.add(goods);
+        List<Object> goodsList = new ArrayList<>();
         List<Type> typeList = new ArrayList<>();
+        for(Map.Entry<String, Goods> entry: dic.entrySet()) {
+            HashMap<String, Object> t = new HashMap<>();
+            t.put("goods", entry.getValue());
+            goodsList.add(t);
+            typeList.add(new Type(entry.getKey(), null, null));
+        }
+
         typeList.add(new Type());
         Map<String, Object> ret = new HashMap<>();
         ret.put("typeList", typeList);
